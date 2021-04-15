2452c9427a8f266e1a7ffb3e15b5ebee091be188
Artemis Georgopoulou <artemis.georgopoulou@gmail.com>
2021-04-15 14:23:31 +0300

    thema9 done

---
+++ thema9.py
@@ -0,0 +1,16 @@
+print((2**100 % 101))
+print((2**50 % 101 )* (2**50 % 101) % 101)
+
+def fast(a, g, N):
+    g_bin = bin(g)
+    x = a
+    d = 1
+    for i in range(len(g_bin)):
+        if g_bin[i] == 1:
+            d = (d*x) % N
+        x = x**2
+    return d
+
+print("fast algorithm: ", fast(2, 100, 101))
+print("fast algorithm: ", fast(2, 1234567, 12345))
+print("fast algorithm: ", fast(130, 7654321, 567))
