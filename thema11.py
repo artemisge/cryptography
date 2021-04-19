a50b54cb2d64598272d272135dd4236b620f16c0
Artemis Georgopoulou <artemis.georgopoulou@gmail.com>
2021-04-19 16:55:07 +0300

    thema11

--- thema11.py
+++ thema11.py
@@ -14,7 +14,6 @@ ASCII τιμές τους (δουλέψτε block by block το C).
 Υποδ. Παραγοντοποιήστε το Ν, κατόπιν υπολογίστε το φ(Ν)...θα χρειαστεί και
 η συνάρτηση fast(). """
 
-msg = "Qz1bNDc0MDYyNjMxOTI2OTM1MDksNTEwNjUxNzgyMDExNzIyMjMsMzAyNjA1NjUyMzUxMjg3MDQsODIzODU5NjMzMzQ0MDQyNjgNCjgxNjkxNTY2NjM5Mjc5MjksNDc0MDYyNjMxOTI2OTM1MDksMTc4Mjc1OTc3MzM2Njk2NDQyLDEzNDQzNDI5NTg5NDgwMzgwNg0KMTEyMTExNTcxODM1NTEyMzA3LDExOTM5MTE1MTc2MTA1MDg4MiwzMDI2MDU2NTIzNTEyODcwNCw4MjM4NTk2MzMzNDQwNDI2OA0KMTM0NDM0Mjk1ODk0ODAzODA2LDQ3NDA2MjYzMTkyNjkzNTA5LDQ1ODE1MzIwOTcyNTYwMjAyLDE3NDYzMjIyOTMxMjA0MTI0OA0KMzAyNjA1NjUyMzUxMjg3MDQsNDc0MDYyNjMxOTI2OTM1MDksMTE5MzkxMTUxNzYxMDUwODgyLDU3MjA4MDc3NzY2NTg1MzA2DQoxMzQ0MzQyOTU4OTQ4MDM4MDYsNDc0MDYyNjMxOTI2OTM1MDksMTE5MzkxMTUxNzYxMDUwODgyLDQ3NDA2MjYzMTkyNjkzNTA5DQoxMTIxMTE1NzE4MzU1MTIzMDcsNTI4ODI4NTEwMjYwNzI1MDcsMTE5MzkxMTUxNzYxMDUwODgyLDU3MjA4MDc3NzY2NTg1MzA2DQoxMTkzOTExNTE3NjEwNTA4ODIsMTEyMTExNTcxODM1NTEyMzA3LDgxNjkxNTY2NjM5Mjc5MjksMTM0NDM0Mjk1ODk0ODAzODA2DQo1NzIwODA3Nzc2NjU4NTMwNiw0NzQwNjI2MzE5MjY5MzUwOSwxODU1ODIxMDUyNzUwNTA5MzIsMTc0NjMyMjI5MzEyMDQxMjQ4DQoxMzQ0MzQyOTU4OTQ4MDM4MDYsODIzODU5NjMzMzQ0MDQyNjgsMTcyNTY1Mzg2MzkzNDQzNjI0LDEwNjM1NjUwMTg5MzU0NjQwMQ0KODE2OTE1NjY2MzkyNzkyOSw0NzQwNjI2MzE5MjY5MzUwOSwxMDM2MTA1OTcyMDYxMDgxNiwxMzQ0MzQyOTU4OTQ4MDM4MDYNCjExOTM5MTE1MTc2MTA1MDg4MiwxNzI1NjUzODYzOTM0NDM2MjQsNDc0MDYyNjMxOTI2OTM1MDksODE2OTE1NjY2MzkyNzkyOQ0KNTI4ODI4NTEwMjYwNzI1MDcsMTE5MzkxMTUxNzYxMDUwODgyLDgxNjkxNTY2NjM5Mjc5MjksNDc0MDYyNjMxOTI2OTM1MDkNCjQ1ODE1MzIwOTcyNTYwMjAyLDE3NDYzMjIyOTMxMjA0MTI0OCwzMDI2MDU2NTIzNTEyODcwNCw0NzQwNjI2MzE5MjY5MzUwOQ0KNTI4ODI4NTEwMjYwNzI1MDcsMTE5MzkxMTUxNzYxMDUwODgyLDExMTUyMzQwODIxMjQ4MTg3OSwxMzQ0MzQyOTU4OTQ4MDM4MDYNCjQ3NDA2MjYzMTkyNjkzNTA5LDExMjExMTU3MTgzNTUxMjMwNyw1Mjg4Mjg1MTAyNjA3MjUwNywxMTkzOTExNTE3NjEwNTA4ODINCjU3MjA4MDc3NzY2NTg1MzA2LDExOTM5MTE1MTc2MTA1MDg4MiwxMTIxMTE1NzE4MzU1MTIzMDcsODE2OTE1NjY2MzkyNzkyOQ0KMTM0NDM0Mjk1ODk0ODAzODA2LDU3MjA4MDc3NzY2NTg1MzA2XQ=="
 N = 11413 #  p * q = N and φ(N) = (p-1)(q-1) p, q prime numbers
 e = 19
 
@@ -31,6 +30,7 @@ def findPrimes(lower, upper):
                 primes.append(num)
     return primes
 
+
 # return two numbers in primes list that if multiplied, they give number N
 def findMultiples(N, primes):
     # to len -1 because we don't want to check the last number with it's self
@@ -40,7 +40,19 @@ def findMultiples(N, primes):
             b = primes[j]
             if a * b == N:
                 return a, b
-        
+
+# a^g mod N = ?
+def fast(a, g, N):
+    g_bin = bin(g)
+    x = a
+    d = 1
+    for i in range(len(g_bin) -1, 1, -1):
+        if (g_bin[i] == '1'):
+            d = (d*x) % N
+        x = (x**2) % N
+    return d 
+
+
 # STEP 1: find p and q
 primes = findPrimes(2, N)
 # p and q found:
@@ -53,3 +65,16 @@ print("And φ(N) =", f_n)
 
 # STEP 3: find d, of private key (N, d)
 # it must be: (e*d) mod f_n == 1
+# using the modular multiplicative inverse
+d = 1179
+print("d=", d)
+
+# STEP 4: decrypt
+C=(3203,909,3143,5255,5343,3203,909,9958,5278,5343,9958,5278,4674,909,
+9958,792,909,4132,3143,9958,3203,5343,792,3143,4443)
+C_dec = []
+for i in range(len(C)):
+    C_dec.append(chr(fast(C[i], d, N)))
+
+
+print(C_dec)
