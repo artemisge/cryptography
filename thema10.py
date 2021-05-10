10ab66a663d0ebd9bee7d0bccd53dc2e698ce301
Artemis Georgopoulou <artemis.georgopoulou@gmail.com>
2021-05-10 22:03:37 +0300

    stuff

--- thema10.py
+++ thema10.py
@@ -1,27 +1,28 @@
 import math
 from math import ceil, sqrt, floor
 
-def search(tbl, y, n):
+def search(tbl, b, p):
     for j in range(len(tbl)):
-        if tbl[j] % n == round(y) % n:
+        if tbl[j] % p == round(b) % p:
             return j
+    return -1
 
-def Shanks(a, n, b):
+def Shanks(g, p, y):
     # g**x = y (mod p), g=2, p=3989, y=2912
     # wikipedia: a**x = b (mod n)
     tbl = []
-    m = math.ceil(math.sqrt(n))
+    m = math.ceil(math.sqrt(p))
 
     for j in range(m):
-        tbl.append(a**j % n)
-    y = b
+        tbl.append(g**j % p)
+    b = y
     for i in range(m):
-        j = search(tbl, y, n)
+        j = search(tbl, b, p)
         if j != -1:
             # print(j, tbl)
             # print()
             return i*m+j
-        y = y * a**(-m)
+        b = b * g**(-m)
     return -1
 
 x = Shanks(2, 3571, 2404)
